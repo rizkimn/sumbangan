6056d2d67e121e3d498d48f5141fff8f534a23ad
rizkimn <rizkimnur0@gmail.com>
2023-02-16 17:15:46 +0900

    favicon

--- index.php
+++ index.php
@@ -30,6 +30,7 @@ if(isset($_POST['getall'])) {
   <meta charset='utf-8'>
   <meta http-equiv='X-UA-Compatible' content='IE=edge'>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600&family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
